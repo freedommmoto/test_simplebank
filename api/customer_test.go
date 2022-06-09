8c62c09abbf2776ff1152893e147fcd47570aa3f
Patara.m <patara.m@cartrack.com>
2022-06-09 15:17:15 +0700

    add mroe test case and fix http worng status case customer not found from 500 to 404

--- api/customer_test.go
+++ api/customer_test.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"bytes"
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	mockdb "github.com/freedommmoto/test_simplebank/db/mock"
@@ -18,25 +19,67 @@ import (
 func TestGetCustomer(t *testing.T) {
 	customer := ranDomMakeCustomer()
 
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
+	testCases := []struct {
+		name          string
+		customerID    int64
+		buildStubs    func(store *mockdb.MockStore)
+		checkResponse func(t *testing.T, recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name:       "OK",
+			customerID: customer.ID,
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().GetCustomer(gomock.Any(), gomock.Eq(customer.ID)).Times(1).Return(customer, nil)
 
-	store := mockdb.NewMockStore(ctrl)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				assert.Equal(t, http.StatusOK, recorder.Code)
+				requireBodyMatchAccount(t, recorder.Body, customer)
+			},
+		},
 
-	store.EXPECT().GetCustomer(gomock.Any(), gomock.Eq(customer.ID)).Times(1).Return(customer, nil)
-	server := NewServer(store)
-	recorder := httptest.NewRecorder()
+		{
+			name:       "NOTFound",
+			customerID: customer.ID,
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().GetCustomer(gomock.Any(), gomock.Eq(customer.ID)).Times(1).Return(db.CustomerAccount{}, sql.ErrNoRows)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				assert.Equal(t, http.StatusNotFound, recorder.Code)
+				//no need to check data is same or not for this case
+				//requireBodyMatchAccount(t, recorder.Body, customer)
+			},
+		},
+		// todo add more cases here
+	}
 
-	//case want to test fail if customer add and select is not the same value
-	//customer.Balance++
-	//t.Logf("customer after mock %v", customer)
+	for i := range testCases {
+		tc := testCases[i]
 
-	url := fmt.Sprintf("/customer/id/%d", customer.ID)
-	request, err := http.NewRequest(http.MethodGet, url, nil)
-	assert.NoError(t, err)
-	server.router.ServeHTTP(recorder, request)
-	assert.Equal(t, http.StatusOK, recorder.Code)
-	requireBodyMatchAccount(t, recorder.Body, customer)
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			store := mockdb.NewMockStore(ctrl)
+			tc.buildStubs(store)
+
+			//store.EXPECT().GetCustomer(gomock.Any(), gomock.Eq(customer.ID)).Times(1).Return(customer, nil)
+			server := NewServer(store)
+			recorder := httptest.NewRecorder()
+
+			//case want to test fail if customer add and select is not the same value
+			//customer.Balance++
+			//t.Logf("customer after mock %v", customer)
+
+			url := fmt.Sprintf("/customer/id/%d", customer.ID)
+			request, err := http.NewRequest(http.MethodGet, url, nil)
+			assert.NoError(t, err)
+			server.router.ServeHTTP(recorder, request)
+			//assert.Equal(t, http.StatusOK, recorder.Code)
+			//requireBodyMatchAccount(t, recorder.Body, customer)
+			tc.checkResponse(t, recorder)
+		})
+	}
 
 }
 
