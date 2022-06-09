35f37667d0508244d3369f771211cd631bf97801
Patara.m <patara.m@cartrack.com>
2022-06-09 14:53:45 +0700

    add mroe detail TestGetCustomer and add case fail when balance is add with int 1

--- api/customer_test.go
+++ api/customer_test.go
@@ -1,12 +1,15 @@
 package api
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	mockdb "github.com/freedommmoto/test_simplebank/db/mock"
 	db "github.com/freedommmoto/test_simplebank/db/sqlc"
 	"github.com/freedommmoto/test_simplebank/tool"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -24,13 +27,16 @@ func TestGetCustomer(t *testing.T) {
 	server := NewServer(store)
 	recorder := httptest.NewRecorder()
 
-	t.Logf("customer after mock %v", customer)
+	//case want to test fail if customer add and select is not the same value
+	//customer.Balance++
+	//t.Logf("customer after mock %v", customer)
 
 	url := fmt.Sprintf("/customer/id/%d", customer.ID)
 	request, err := http.NewRequest(http.MethodGet, url, nil)
 	assert.NoError(t, err)
 	server.router.ServeHTTP(recorder, request)
 	assert.Equal(t, http.StatusOK, recorder.Code)
+	requireBodyMatchAccount(t, recorder.Body, customer)
 
 }
 
@@ -40,4 +46,15 @@ func ranDomMakeCustomer() db.CustomerAccount {
 		CustomerName: tool.RandomOwner(),
 		Balance:      tool.RandomMoney(),
 	}
+}
+
+func requireBodyMatchAccount(t *testing.T, body *bytes.Buffer, customer db.CustomerAccount) {
+	data, err := ioutil.ReadAll(body)
+	assert.NoError(t, err)
+
+	var gotCustomer db.CustomerAccount
+	err = json.Unmarshal(data, &gotCustomer)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, gotCustomer)
+	assert.Equal(t, customer, gotCustomer)
 }
