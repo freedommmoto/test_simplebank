bd13d8c358bde3b2597f2bdd872fb24850b05e56
Patara.m <patara.m@cartrack.com>
2022-06-19 09:43:33 +0700

    add basic stuct ready

--- go.mod
+++ go.mod
@@ -18,6 +18,7 @@ require (
 	github.com/go-playground/validator/v10 v10.11.0 // indirect
 	github.com/golang/mock v1.6.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
---
+++ token/payload.go
@@ -0,0 +1,27 @@
+package token
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+type Payload struct {
+	ID        uuid.UUID `json:"id"`
+	Username  string    `json:"username"`
+	IssuedAt  time.Time `json:"issued_at"`
+	ExpiredAt time.Time `json:"expired_at"`
+}
+
+func NewPayload(username string, duration time.Duration) (*Payload, error) {
+	tokenId, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+	payload := &Payload{
+		ID:        tokenId,
+		Username:  username,
+		IssuedAt:  time.Now(),
+		ExpiredAt: time.Now().Add(duration),
+	}
+	return payload, nil
+}
